e901c45385c6106a6b7d6378b81b55f7ee4f41e5
Diana Carrillo <Diana@MacBook-Pro-de-Diana.local>
2018-11-30 19:16:05 -0600

    subiendo confirm de remove function

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import firebase from 'firebase';
-import { Card, Button } from 'react-materialize'
+import { Card, Button, Form } from 'react-materialize'
 
 class Posts extends Component{
     constructor() {
@@ -21,38 +21,52 @@ class Posts extends Component{
       })
 }
     handleRemove = (e) =>{
-        firebase.database().ref('new').child(e.target.dataset.key).remove();
+       const confirm = window.confirm('¿Estás seguro de eliminar éste mensaje?')
+        if (confirm === true){
+            firebase.database().ref('new').child(e.target.dataset.key).remove();
+       } else {
+            return;
+       }
 
+    }
+    handleEdit = () =>{
+       alert('I will edit LOL') 
     }
 
 
-render(){
-// console.log(this.state.posts)
-    return(
-             <div className="post-container">
-                {this.state.posts.map(post=>{
-                    // console.log(post);
-                return(
-                    <Card  className="card-content" className={post.keyPost} >
-                        <div className="post-content"  >
-                        <img src={post.photoUrl} width="10%" alt="" className="circle img-user"/>
-                            <p className="user-name-post">{post.creatorName}</p>
-                            <p id={post.keyPost} className="post-message">{post.message}</p>
-                      </div>
-                      <div className ="card-action card-footer">
-                      
-                            <Button type= "button" data-key={post.keyPost} className= "edit-message-btn"> Editar </Button>
-                            <Button type="button" onClick={this.handleRemove}  data-key={post.keyPost} className="delete-message-btn"> Borrar </Button>
-                            <p id ={post.keyPost} className="favorite-counter right"></p><i data-key={post.keyPost} className="small material-icons right favoriteCounter">favorite</i>
-                      </div> 
-                  </Card>
-                        )
-                      })
-                    }
-              </div>
-    )
-  }
-}
+    render(){
+    // console.log(this.state.posts)
+        return(
+                <div className="post-container">
+                    {this.state.posts.map(post=>{
+                        // console.log(post);
+                    return(
+                        <Card  className="card-content" className={post.keyPost} >
+                            {/* <Form>
+                                <input />
+                                <Button>Guardar</Button>                                
+                                <Button>Cancelar</Button>
+                            </Form> */}
+                            
+                            <div className="post-content"  >
+                            <img src={post.photoUrl} width="10%" alt="" className="circle img-user"/>
+                                <p className="user-name-post">{post.creatorName}</p>
+                                <p id={post.keyPost} className="post-message">{post.message}</p>
+                        </div>
+                        <div className ="card-action card-footer">
+                        
+                                <Button type= "button" onClick={this.handleEdit} data-key={post.keyPost} className= "edit-message-btn"> Editar </Button>
+                                <Button type="button" onClick={this.handleRemove}  data-key={post.keyPost} className="delete-message-btn"> Borrar </Button>
+                                <p id ={post.keyPost} className="favorite-counter right"></p><i data-key={post.keyPost} className="small material-icons right favoriteCounter">favorite</i>
+                        </div> 
+                    </Card>
+                            )
+                        })
+                        }
+                </div>
+        )
+    }
+    }
 
 
 export default Posts;
